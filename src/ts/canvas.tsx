e84ea67ae8377a213ea8650925306f58c64d65e6
xharris <xhh950@gmail.com>
2021-07-23 09:47:24 -0400

    now using react-pixi

--- src/ts/canvas.tsx
+++ src/ts/canvas.tsx
@@ -1,8 +1,8 @@
-import { useCallback, useEffect, useLayoutEffect, useRef, useState } from "react"
-import { bem,  useWindowSize } from "ts/ui"
+import { useCallback, useState } from "react"
+import { bem,  useWindowSize, ObjectAny } from "ts/ui"
 import { useGlobalCtx } from "./globalcontext"
 import * as PIXI from "pixi.js"
-// import { Stage, Container, Sprite } from "@inlet/react-pixi"
+import { Stage, Container, Graphics, Text, PixiComponent } from "@inlet/react-pixi"
 // import { useProject } from "./project"
 
 type TileCropInfo = { path:string, x:number, y:number, w:number, h:number }
@@ -26,7 +26,7 @@ type CanvasCtx = {
 const bss = bem("canvas")
 
 export const useCanvasCtx = () => {
-  const { data:{ tiles, layers, current_layer }, update } = useGlobalCtx<CanvasCtx>("canvas")
+  const { data:{ tiles, layers = {}, current_layer }, update } = useGlobalCtx<CanvasCtx>("canvas")
 
   const setSelectedTiles = useCallback((...args:TileCropInfo[]) => {
     update({ tiles:args })
@@ -81,67 +81,27 @@ export const useCanvasCtx = () => {
   return { 
     tiles, 
     layers, 
-    current_layer:current_layer && layers[current_layer],
+    current_layer,
     layer_container:current_layer && layers[current_layer]._container, 
     setSelectedTiles, addLayer, removeLayer, setLayer, updateLayer,
     setCurrentLayer
   }
 }
 
-export const Canvas = () => {
-  const el_canvas = useRef<HTMLDivElement>()
-  const [width, height] = useWindowSize()
-  // const { assets } = useProject()
-  const { tiles, layers, layer_container, current_layer } = useCanvasCtx()
-  // PIXIjs
-  // const [textures, setTextures] = useState<ObjectAny<PIXI.Texture>>({})
-  const [app, setApp] = useState<PIXI.Application>()
-  const [grid, setGrid] = useState<PIXI.Graphics>()
-  const [tileContainer, setTileContainer] = useState<PIXI.Container>()
-  // const [overlayContainer, setOverlayContainer] = useState<PIXI.Container>()
-  // const [infoText, setInfoText] = useState<PIXI.Text>()
+interface ITile {
+  key:string,
+  x:number, 
+  y:number,
+  tile:TileCropInfo
+}
 
-  // setup pixi.js
-  useEffect(() => {
-    if (el_canvas.current && !app) {
-      let new_app = new PIXI.Application({ 
-        backgroundColor:0xEEEEEE
-      })
-      el_canvas.current.appendChild(new_app.view)
-      
-      let new_grid = new PIXI.Graphics()
-      let new_tile = new PIXI.Container()
-      let new_overlay = new PIXI.Container()
-
-      new_app.stage.addChild(new_grid, new_tile, new_overlay)
-      setTileContainer(new_tile)
-      // setOverlayContainer(new_overlay)
-
-      setApp(new_app)
-      setGrid(new_grid) 
-    }
-  }, [el_canvas, app, width, height])
-
-  useEffect(() => {
-    updateGrid()
-  }, [current_layer])
-
-  useEffect(() => {
-    if (tileContainer && layers)
-      Object.values(layers)
-        .filter(layer => !layer._container.parent)
-        .forEach(layer => tileContainer.addChild(layer._container))
-  }, [layers, tileContainer])
-
-  useLayoutEffect(() => {
-    updateCanvasSize()
-  }, [width, height, app])
-
-  const getSprite = useCallback((tile:TileCropInfo) => {
-    let new_spr = new PIXI.Sprite()
+const Tile = PixiComponent<ITile, PIXI.Sprite>("Tile", {
+  create: () => new PIXI.Sprite(),
+  applyProps: (inst, _, { x, y, tile }) => {
+    // load texture
     PIXI.Texture.fromURL(`file://${tile.path}`)
       .then(texture => {
-        new_spr.texture = texture.clone()
+        inst.texture = texture.clone()
 
         let {x, y, w, h} = tile
 
@@ -152,45 +112,51 @@ export const Canvas = () => {
         if (y + h > texture.height)
           h -= (y + h) % texture.height
 
-        new_spr.texture.frame = new PIXI.Rectangle(x, y, w, h)
+        inst.texture.frame = new PIXI.Rectangle(x, y, w, h)
       })
-    return new_spr
-  }, [])
+    inst.x = x 
+    inst.y = y
+  }
+})
 
-  const updateGrid = useCallback(() => {
+export const Canvas = () => {
+  const [width, height] = useWindowSize()
+  // const { assets } = useProject()
+  const { tiles, layers, current_layer } = useCanvasCtx()
+  const [placedTiles, setPlacedTiles] = useState<ObjectAny<ITile[]>>({})
+
+  const drawGrid = useCallback(grid => {
     if (grid && current_layer) {
+      const layer = layers[current_layer]
       grid.clear()
       grid.lineStyle({ width:1, color:0x212121, alpha:0.1 })
 
-      let offx = current_layer.offset.x % current_layer.snap.x
-      let offy = current_layer.offset.y % current_layer.snap.y
+      let offx = layer.offset.x % layer.snap.x
+      let offy = layer.offset.y % layer.snap.y
 
-      if (current_layer.snap.x > 3)
-        for (let x = offx; x < width + offx; x += current_layer.snap.x) {
+      if (layer.snap.x > 3)
+        for (let x = offx; x < width + offx; x += layer.snap.x) {
           grid.moveTo(x, 0)
           grid.lineTo(x, height)
         }
-      if (current_layer.snap.y > 3)
-        for (let y = offy; y < height + offy; y += current_layer.snap.y) {
+      if (layer.snap.y > 3)
+        for (let y = offy; y < height + offy; y += layer.snap.y) {
           grid.moveTo(0, y)
           grid.lineTo(width, y)
         }
     }
-  }, [width, height, grid, current_layer])
-
-  const updateCanvasSize = useCallback(() => {
-    if (app) {
-      app.renderer.resize(width, height)
-      updateGrid()
-    }
-  }, [width, height, app, updateGrid])
+  }, [width, height, current_layer])
 
   const onPlace = useCallback((add:boolean, x:number, y:number) => {
-    if (add && tiles && layer_container && current_layer) {
+    if (add && tiles && current_layer) {
       let minx:number, miny:number
-      x -= x % current_layer.snap.x 
-      y -= y % current_layer.snap.y
-      tiles 
+
+      // snap position
+      const layer = layers[current_layer]
+      x -= x % layer.snap.x
+      y -= y % layer.snap.y
+
+      let new_tiles = tiles 
         // calculate x/y offsets
         .map(tile => {
           if (minx == null || tile.x < minx)
@@ -200,19 +166,23 @@ export const Canvas = () => {
           return tile
         })
         // place the tiles 
-        .forEach(tile => {
-          const spr_tile = getSprite(tile)
-          spr_tile.x = x + (tile.x - minx)
-          spr_tile.y = y + (tile.y - miny)
-          layer_container.addChild(spr_tile)
-        })
+        .map(tile => ({ 
+            x: x + (tile.x - minx), 
+            y: y + (tile.y - miny), 
+            tile 
+        }))
+        .map(tile => ({
+          ...tile,
+          key: `${current_layer},${tile.x},${tile.y}`
+        }))
+      const oldTiles = (placedTiles[current_layer] || []).filter(t => !new_tiles.some(nt => nt.key === t.key))
+      setPlacedTiles({ ...placedTiles, [current_layer]:[ ...oldTiles, ...new_tiles ]})
     }
-  }, [layer_container, current_layer, tiles])
+  }, [current_layer, tiles, placedTiles, setPlacedTiles])
 
   return (
     <div 
       className={bss()}
-      ref={el_canvas} 
       onMouseDown={e => 
         onPlace(e.buttons === 1, e.clientX, e.clientY)
       }
@@ -220,6 +190,37 @@ export const Canvas = () => {
         if (e.buttons !== 0)
           onPlace(e.buttons === 1, e.clientX, e.clientY)
       }}
-    />
+    >
+      <Stage 
+        width={width} 
+        height={height}
+        options={{
+          backgroundColor: 0xEEEEEE
+        }}
+      >
+        {/* grid */}
+        <Container>
+          <Graphics draw={drawGrid} />
+        </Container>
+        {/* layers */}
+        <Container>
+          {Object.keys(placedTiles)
+            .sort((a, b) => layers[a].z - layers[b].z)
+            .map(id => (
+              <Container key={id}>
+                {placedTiles[id].map(tile => (
+                  <Tile {...tile}/>
+                ))}
+              </Container>
+            ))}
+        </Container>
+        {/* overlay ui */}
+        <Container>
+          <Text 
+          
+          />
+        </Container>
+      </Stage>
+    </div>
   )
 }
